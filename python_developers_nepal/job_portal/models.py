4a36689ccd1cab2feb1a9a562b78c18906667955
sonish <sonishmzn95@gmail.com>
2020-11-08 13:10:14 +0545

    model updated

--- python_developers_nepal/job_portal/models.py
+++ python_developers_nepal/job_portal/models.py
@@ -28,12 +28,15 @@ class Company(models.Model):
     company_logo = models.ImageField(upload_to="company_logo")
     created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
 
+    def __str__(self):
+        return self.company_name
+
 
 class HirePost(models.Model):
     posted_by_id = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    company_id = models.ForeignKey(Company, on_delete=models.CASCADE)
-    hire_post_title = models.CharField(max_length=255)
-    hire_post_description = models.TextField(max_length=2000)
+    company_id = models.ForeignKey(Company, verbose_name="Company", on_delete=models.CASCADE)
+    hire_post_title = models.CharField("Position Title",max_length=255)
+    hire_post_description = models.TextField("Position Description",max_length=2000)
     apply_before = models.DateTimeField()
     location = models.CharField(max_length=100)
     is_verified = models.BooleanField(default=False)
@@ -47,7 +50,14 @@ class HirePost(models.Model):
 class InternPost(HirePost):
     intern_type = models.CharField(max_length=50, choices=INTERN_TYPE, default='PAID')
 
+    def __str__(self):
+        return self.hire_post_title
+
 
 class JobPost(HirePost):
     job_type = models.CharField(max_length=50, choices=JOB_TYPE)
-    job_position = models.CharField(max_length=50, choices=JOB_TYPE)
+    job_position = models.CharField(max_length=50, choices=JOB_POSITION)
+
+    def __str__(self):
+        return self.hire_post_title
+
