65a6f9f435ad490c880affdc1cdd5fdfb845baa3
sonish <sonishmzn95@gmail.com>
2020-11-08 13:12:44 +0545

    model registered in admin

--- python_developers_nepal/job_portal/admin.py
+++ python_developers_nepal/job_portal/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from . import models
 # Register your models here.
+
+admin.site.register(models.Company)
+admin.site.register(models.JobPost)
+admin.site.register(models.InternPost)
