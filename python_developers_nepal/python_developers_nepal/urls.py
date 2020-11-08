7df9e3708584fa6f9b579758a3417d3e889e9192
sonish <sonishmzn95@gmail.com>
2020-11-08 15:51:56 +0545

    url fixed

--- python_developers_nepal/python_developers_nepal/urls.py
+++ python_developers_nepal/python_developers_nepal/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('job-portal', include("job_portal.urls")),
+    path('job-portal/', include("job_portal.urls")),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
